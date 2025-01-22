f707aa261260e6150a8d3dd6408cfc60dca43b62
Dan Butuc <146419517+danbutuc2@users.noreply.github.com>
2025-01-22 10:42:46 +0100

    Create AppTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+import com.example.Application;
+import org. junit.Test;
+import static org. junit.Assert.*;
+
+public class AppTest {
+  
+  @Test
+  public void testApp) {
+    Application myApp = new Application®;
+    
+    String result = myApp. getStatus;
+    
+    assertEquals("0K", result);
+  }
+
+}
