8bae2c2c15dcbcef7aa023896683b7920c74096f
Dan Butuc <146419517+danbutuc2@users.noreply.github.com>
2025-01-22 10:52:30 +0100

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,16 +1,16 @@
 import com.example.Application;
-import org. junit.Test;
-import static org. junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
-  
-  @Test
-  public void testApp) {
-    Application myApp = new Application®;
-    
-    String result = myApp. getStatus;
-    
-    assertEquals("0K", result);
-  }
+
+    @Test
+    public void testApp() {
+        Application myApp = new Application();
+
+        String result = myApp.getStatus();
+
+        assertEquals("OK", result);
+    }
 
 }
