4ce60a0425142f886c903908b3b55a0ef1488b32
Dan Butuc <146419517+danbutuc2@users.noreply.github.com>
2025-01-23 13:08:45 +0100

    Update2 script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t danbutuc/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push danbutuc/demo-app:jma-2.0'
     }
 } 
